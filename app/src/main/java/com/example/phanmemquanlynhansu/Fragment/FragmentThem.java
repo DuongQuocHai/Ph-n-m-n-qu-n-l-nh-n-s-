a32106160926421a0596046805b6933ac5bb027e
Duong Quoc Hai <duongquochai123456@gmail.com>
2019-12-06 16:26:50 +0700

    pull code

--- app/src/main/java/com/example/phanmemquanlynhansu/Fragment/FragmentThem.java
+++ app/src/main/java/com/example/phanmemquanlynhansu/Fragment/FragmentThem.java
@@ -1,31 +1,23 @@
 package com.example.phanmemquanlynhansu.Fragment;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.phanmemquanlynhansu.CaLamActivity;
-import com.example.phanmemquanlynhansu.ImageConverter;
 import com.example.phanmemquanlynhansu.NhanVienActivity;
 import com.example.phanmemquanlynhansu.R;
-
-import java.text.ParseException;
 
 public class FragmentThem extends Fragment {
     View view;
     LinearLayout btnNhanVien,btnCaLam;
-    LinearLayout nhanVien;
 
     @Nullable
     @Override
@@ -33,17 +25,11 @@ public class FragmentThem extends Fragment {
         view = inflater.inflate(R.layout.fragment_them,container,false);
         addControl();
         addEvent();
-
-
-
-
-
         return view;
     }
     public void addControl(){
         btnNhanVien = view.findViewById(R.id.btn_nhanvien_fgthem);
         btnCaLam = view.findViewById(R.id.btn_calam_fgthem);
-
     }
     public void addEvent(){
         btnNhanVien.setOnClickListener(new View.OnClickListener() {
@@ -61,7 +47,7 @@ public class FragmentThem extends Fragment {
             }
         });
 
-        
+
     }
 
 
